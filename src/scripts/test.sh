d2ceaef14625c565695b2dffbf30bed3a19d0702
ChangyWen <ddcwen@gmail.com>
2025-10-18 20:13:52 +0800

    chore: minor

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -1,9 +1,9 @@
 set -x
 
 source_dir=/mnt/blob_output/v-dachengwen/TruthRL/logs_math-grpo-1760692940
-for i in 40 80 120; do
-# for i in 160 200; do
-    if [ -f $source_dir/global_step_${i}/actor/huggingface/model-00002-of-00002.safetensors ]; then
+# for i in 40 80 120; do
+for i in 160 200; do
+    if [ -f $source_dir/global_step_${i}/actor/huggingface/model.safetensors ]; then
         echo "model has been merged already"
     else
         echo "model not merged, merging model ..."
